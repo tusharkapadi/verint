5802c6fe509c7011bce2b0a3c6c6ac57dc2d9bd8
Tushar Kapadi <tushar.kapadi@gmail.com>
2025-07-02 08:21:17 -0700

    Update main.tf

--- env2/main.tf
+++ env2/main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "my_first_ec2_instance" {
+resource "aws_instance" "my_first_ec2_instance_2" {
   ami = "ami-0418306302097dbff" 
   instance_type = "t2.micro" # Got to https://aws.amazon.com/ec2/instance-types/t2/ for a full T2 instance type list.
   
   tags = {
-    Name = "my_first_ec2_instance"
+    Name = "my_first_ec2_instance_2"
   }
 }
