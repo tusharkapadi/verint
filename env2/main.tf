468071dd53ce7c706343b4728dabb5849d86f619
tusharkapadi <tushar.kapadi@gmail.com>
2025-07-16 07:22:06 -0700

    Remove backend configuration

--- env2/main.tf
+++ env2/main.tf
@@ -1,10 +1,5 @@
 terraform {
-    backend "s3" {
-        bucket = "tk-tf-migration-bucket"
-        key = "env2/terraform.tfstate"
-        region = var.aws_region
     }
-}
 
 provider "aws" {
   region = var.aws_region
